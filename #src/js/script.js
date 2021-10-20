9522ad3aef7aa0fab071d4c52c0d3fda3e61e395
WebDev <webdev@email.com>
2021-10-20 12:15:54 +0300

    Started from an empty template

---
+++ #src/js/script.js
@@ -0,0 +1,17 @@
+"use strict"
+
+// Прелоадер
+
+// window.onload = function () {
+//     document.body.classList.add('loaded-hiding');
+//     document.body.style.overflow = 'hidden';
+//     window.setTimeout(function () {
+//         document.body.classList.add('loaded');
+//         document.body.classList.remove('loaded-hiding');
+//         document.body.style.overflow = 'auto';
+//     }, 500);
+// }
+
+window.addEventListener('DOMContentLoaded', function () {
+
+})
